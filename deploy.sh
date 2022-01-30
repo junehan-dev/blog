45930eedb0c039f15e54ee7b28d962185988b067
junehan <junehan.dev@gmail.com>
2022-01-31 02:03:09 +0900

    rebuilding site Mon Jan 31 02:03:03 AM KST 2022

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+hugo -t hugo-tranquilpeak-theme
+cd public
+git add .
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+	then msg="$1"
+fi
+git commit -m "$msg"
+git push origin main
+cd ..
+git add .
+git commit -m "$msg"
+git push origin main
