6b6d11130c9b2a06fa4f98bb238379c4b77d6dcb
junehan <junehan.dev@gmail.com>
2022-06-10 17:19:59 +0900

    Ignore Fixed

--- 1-make_post.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z $1 ]
-then
-	echo "no input"
-else
-	hugo new post/$1
-fi
--- postscripts/diarymake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-basepath=~/Documents/POSTINGS/hugo_blog-Diary_Study 
-diary_path=${basepath}/content/post/DIARY
-today=$( date -I )
-hugo new ${diary_path}/${today}.md
-date=$( cat ${diary_path}/${today}.md | grep date )
-cat ${basepath}/BASES/DIARY >> ${diary_path}/${today}.md
-vim ${diary_path}/${today}.md
